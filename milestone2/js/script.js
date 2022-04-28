349f2b502a76059ff368ae02763b9ab62a628600
SanSciain <mastroeni.santi@gmail.com>
2022-04-28 13:50:44 +0200

    Add: all milestone2

---
+++ milestone2/js/script.js
@@ -0,0 +1,45 @@
+const team = [
+    {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+    },
+    {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+
+
+for (let i=0; i<team.length;i++){
+    const container = document.querySelector(".container");
+    const infoContainer = document.createElement("p");
+    console.log(infoContainer);
+    let person = team[i];
+    for (let key in person){
+        infoContainer.append(person[key]);
+    }
+    container.append(infoContainer);
+}
